c68969a18f3c055566f300fe0358a42553946b9a
Pierce Thompson <insprill@gmail.com>
2023-02-14 06:59:20 -0500

    Simplify modifying bogie meshes, operate on sharedMesh instead of mesh
    Changes bogie behavior to resize all children meshes. This is most likely wanted behaviour, and allows CCL cars with custom bogies to be resized.
    
    It also operates on the shared mesh instead of the mesh to avoid rebuilding the same mesh many times.

--- Patches/BogiePatch.cs
+++ Patches/BogiePatch.cs
@@ -1,4 +1,5 @@
-﻿using Gauge.MeshModifiers;
+﻿using System.Collections.Generic;
+using Gauge.MeshModifiers;
 using HarmonyLib;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ namespace Gauge.Patches
     [HarmonyPatch(typeof(Bogie), "Start")]
     public static class Bogie_Start_Patch
     {
+        private static readonly HashSet<Mesh> modifiedMeshes = new HashSet<Mesh>();
+
         public static void Postfix(Bogie __instance)
         {
             if (Main.Settings.gauge == Gauge.Standard)
@@ -14,42 +17,10 @@ namespace Gauge.Patches
 
             foreach (MeshFilter filter in __instance.gameObject.GetComponentsInChildren<MeshFilter>())
             {
-                // TODO: Modify the shared mesh instead of this wackiness.
-                Mesh mesh = filter.mesh;
-                string meshName = mesh.name.Replace(" Instance", "");
-                switch (meshName)
-                {
-                    // SH282 leading/trailing wheels
-                    // case "ext Wheels Rear": // Clips through the frame
-                    case "ext Wheels Front":
-                    // DE6 wheels
-                    case "diesel_bogie_wheels":
-                    case "diesel_bogie_wheels_LOD1":
-                    // DE6 bogies
-                    case "diesel_bogie_frame":
-                    case "diesel_bogie_frame_LOD1":
-                    case "diesel_bogie_frame_LOD2":
-                    case "diesel_bogie_frame_LOD3":
-                    case "diesel_bogie_LOD4":
-                    // DE2 wheels
-                    case "ext axle_F":
-                    case "ext axle_R":
-                    // Standard wheels
-                    case "axle_LOD0":
-                    case "axle_LOD1":
-                    // Brake rigging
-                    case "Bogie2Brakes02":
-                    case "Bogie2Brakes01":
-                    case "Bogie2BrakesCylinder":
-                    // Standard bogies
-                    case "bogie2_LOD0":
-                    case "bogie2_LOD1":
-                    case "bogie2_LOD2":
-                    case "bogie2_LOD3":
-                    case "bogie2_LOD4":
-                        Axle.ModifyMesh(mesh);
-                        break;
-                }
+                Mesh mesh = filter.sharedMesh;
+                if (!mesh.isReadable || modifiedMeshes.Contains(mesh)) continue;
+                Axle.ModifyMesh(mesh);
+                modifiedMeshes.Add(mesh);
             }
         }
     }
