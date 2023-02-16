96bed3ed79dac5b21d6f9ac3929e1ddd8d416ed8
Pierce Thompson <insprill@gmail.com>
2023-02-16 02:26:13 -0500

    Store static switch verts as ushorts instead of shorts

--- MeshModifiers/StaticSwitch.cs
+++ MeshModifiers/StaticSwitch.cs
@@ -6,14 +6,14 @@ namespace Gauge.MeshModifiers
     public static class StaticSwitch
     {
         private const float GAUGE_DIFF_OFFSET = 0.01f; // No idea why we need this, but switches are misaligned without it.
-        private const short VERT_COUNT = 1370;
+        private const ushort VERT_COUNT = 1370;
 
         // Vertices going clockwise. All verts are duplicated for the edges of the rail, and the outline of the end
-        private static readonly short[] DIVERGING_EXTEND_VERTS = {
+        private static readonly ushort[] DIVERGING_EXTEND_VERTS = {
             18, 17, 19, 21, 23, 25, 35, 40, 43, 48, 55, 60, 62, 77, 79, 85, 90, 93, 98,
             204, 207, 208, 210, 212, 215, 216, 214, 219, 218, 217, 222, 220, 221, 213, 211, 209, 205, 206
         };
-        private static readonly short[] STRAIGHT_EXTEND_VERTS = {
+        private static readonly ushort[] STRAIGHT_EXTEND_VERTS = {
             308, 301, 277, 269, 264, 260, 257, 253, 250, 246, 243, 241, 240, 238, 233, 230, 227, 226, 223,
             325, 328, 329, 331, 333, 336, 337, 335, 340, 339, 338, 343, 341, 342, 334, 332, 330, 326, 327
         };
@@ -29,8 +29,6 @@ namespace Gauge.MeshModifiers
 
             float gaugeDiff = Main.Settings.gauge.GetDiffToStandard() + GAUGE_DIFF_OFFSET;
             float zOffset = gaugeDiff * 16;
-
-            // https://blender.stackexchange.com/a/3253
 
             // Split
             for (int i = 0; i < 344; i++)
