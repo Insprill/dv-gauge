f92262301fdea26a7a63415620242da21d171897
Pierce Thompson <insprill@gmail.com>
2023-02-17 05:49:34 -0500

    Ensure DE2 axles stay in the suspension on narrower gauges

--- Patches/BogiePatch.cs
+++ Patches/BogiePatch.cs
@@ -9,6 +9,10 @@ namespace Gauge.Patches
     [HarmonyPatch(typeof(Bogie), "Start")]
     public static class Bogie_Start_Patch
     {
+        private static readonly ushort[] DE2_AXLE_SKIP_VERTS = {
+            264, 267, 269, 271, 273, 275, 277, 279, 281, 283, 285, 287, 289, 291, 293, 295, 297, 628, 631, 633, 635, 637, 639, 641, 643, 645, 647, 649, 651, 653, 655, 657, 659, 661
+        };
+
         private static readonly HashSet<Mesh> modifiedMeshes = new HashSet<Mesh>();
 
         public static void Postfix(Bogie __instance)
@@ -20,7 +24,16 @@ namespace Gauge.Patches
             {
                 Mesh mesh = filter.sharedMesh;
                 if (!mesh.isReadable || modifiedMeshes.Contains(mesh)) continue;
-                Symmetrical.ScaleToGauge(mesh, true, __instance.Car.GetGauge());
+                switch (mesh.name)
+                {
+                    case "ext axle_F":
+                    case "ext axle_R":
+                        Symmetrical.ScaleToGauge(mesh, true, __instance.Car.GetGauge(), DE2_AXLE_SKIP_VERTS);
+                        break;
+                    default:
+                        Symmetrical.ScaleToGauge(mesh, true, __instance.Car.GetGauge());
+                        break;
+                }
                 modifiedMeshes.Add(mesh);
             }
         }
