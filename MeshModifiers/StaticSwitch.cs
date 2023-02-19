2c9388d97406cf711cbf0127c46ea8d38a829cd3
Pierce Thompson <insprill@gmail.com>
2023-02-19 07:58:21 -0500

    Cleanup StaticSwitch vertices

--- MeshModifiers/StaticSwitch.cs
+++ MeshModifiers/StaticSwitch.cs
@@ -7,14 +7,14 @@ namespace Gauge.MeshModifiers
     {
         public const float Z_OFFSET_FACTOR = 18;
 
-        // Vertices going clockwise. All verts are duplicated for the edges of the rail, and the outline of the end
+        private const ushort SPLIT_MAX_VERT = 344;
+        private const ushort RIGHT_RAIL_MAX_VERT = 381;
+        private const ushort LEFT_RAIL_MAX_VERT = 1370;
         private static readonly ushort[] DIVERGING_EXTEND_VERTS = {
-            18, 17, 19, 21, 23, 25, 35, 40, 43, 48, 55, 60, 62, 77, 79, 85, 90, 93, 98,
-            204, 207, 208, 210, 212, 215, 216, 214, 219, 218, 217, 222, 220, 221, 213, 211, 209, 205, 206
+            17, 18, 19, 21, 23, 25, 35, 40, 43, 48, 55, 60, 62, 77, 79, 85, 90, 93, 98, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222
         };
         private static readonly ushort[] STRAIGHT_EXTEND_VERTS = {
-            308, 301, 277, 269, 264, 260, 257, 253, 250, 246, 243, 241, 240, 238, 233, 230, 227, 226, 223,
-            325, 328, 329, 331, 333, 336, 337, 335, 340, 339, 338, 343, 341, 342, 334, 332, 330, 326, 327
+            223, 226, 227, 230, 233, 238, 240, 241, 243, 246, 250, 253, 257, 260, 264, 269, 277, 301, 308, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343
         };
 
         public static void ModifyMesh(Mesh mesh)
@@ -25,7 +25,7 @@ namespace Gauge.MeshModifiers
             float zOffset = gaugeDiff * Z_OFFSET_FACTOR;
 
             // Split
-            for (int i = 0; i < 344; i++)
+            for (int i = 0; i < SPLIT_MAX_VERT; i++)
             {
                 verts[i].x -= gaugeDiff;
                 verts[i].z += zOffset;
@@ -40,10 +40,10 @@ namespace Gauge.MeshModifiers
             foreach (ushort idx in STRAIGHT_EXTEND_VERTS) verts[idx].z -= zOffset;
 
             // Right rail
-            for (int i = 344 - 1; i < 381; i++) verts[i].x += gaugeDiff;
+            for (int i = SPLIT_MAX_VERT - 1; i < RIGHT_RAIL_MAX_VERT; i++) verts[i].x += gaugeDiff;
 
             // Left rail
-            for (int i = 382 - 1; i < 1370; i++) verts[i].x -= gaugeDiff;
+            for (int i = RIGHT_RAIL_MAX_VERT; i < LEFT_RAIL_MAX_VERT; i++) verts[i].x -= gaugeDiff;
 
             mesh.ApplyVerts(verts);
         }
