4d2d7e8e557abd2c96e80b428d5c1e5f9bf8cc23
Pierce Thompson <insprill@gmail.com>
2023-03-15 04:14:41 -0400

    Update package.sh

--- package.sh
+++ package.sh
@@ -8,11 +8,12 @@ function check_file_exists() {
 }
 
 INFO_FILE="info.json"
-GAUGE_DLL="build/Gauge.dll"
+GAUGE_DLL="bin/Release/netframework4.8/Gauge.dll"
+ASSETBUNDLE="gauge.assetbundle"
 DISPLAY_NAME=$(jq -r '.DisplayName' $INFO_FILE)
 VERSION=$(jq -r '.Version' $INFO_FILE)
 
 check_file_exists "$GAUGE_DLL"
 check_file_exists "$INFO_FILE"
 
-zip -1 -T -j -u "${DISPLAY_NAME}_v$VERSION.zip" $GAUGE_DLL $INFO_FILE
+zip -1 -T -j -u "${DISPLAY_NAME}_v$VERSION.zip" $GAUGE_DLL $INFO_FILE $ASSETBUNDLE
