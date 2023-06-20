6cb74d333331062872db21a5f6b879e0bca7f2fe
Pierce Thompson <insprill@gmail.com>
2023-06-20 00:23:04 -0400

    Copy Mapify's packaging system

---
+++ package.ps1
@@ -0,0 +1,33 @@
+param (
+    [switch]$NoArchive,
+    [string]$OutputDirectory = $PSScriptRoot
+)
+
+Set-Location "$PSScriptRoot"
+
+$DistDir = "$OutputDirectory/dist"
+if ($NoArchive)
+{
+    $ZipWorkDir = "$OutputDirectory"
+}
+else
+{
+    $ZipWorkDir = "$DistDir/tmp"
+}
+$ZipRootDir = "$ZipWorkDir/BepInEx"
+$ZipInnerDir = "$ZipRootDir/plugins/Mapify/"
+$RuntimeBuildDir = "build/runtime"
+$LicenseFile = "LICENSE"
+$LocaleFile = "locale.csv"
+$MapifyDll = "$RuntimeBuildDir/Mapify.dll"
+$MapifyEditorDll = "$RuntimeBuildDir/MapifyEditor.dll"
+
+New-Item "$ZipInnerDir" -ItemType Directory -Force
+Copy-Item -Force -Path $LicenseFile, $LocaleFile, $MapifyDll, $MapifyEditorDll -Destination $ZipInnerDir
+
+if (!$NoArchive)
+{
+    $VERSION = (Select-String -Pattern '([0-9]+\.[0-9]+\.[0-9]+)' -Path Mapify/Mapify.cs).Matches.Value
+    $FILE_NAME = "$DistDir/Mapify_v$VERSION.zip"
+    Compress-Archive -Update -CompressionLevel Fastest -Path "$ZipRootDir" -DestinationPath "$FILE_NAME"
+}
