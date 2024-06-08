c54acb6f152c0867bdfae19d74b8436d131a2f6f
WhistleWiz <wizardmarcos@gmail.com>
2024-06-08 22:21:49 +0100

    Update RailQuality.cs

--- Gauge/RailQuality.cs
+++ Gauge/RailQuality.cs
@@ -11,25 +11,34 @@ namespace Gauge
     public struct RailQuality
     {
         public static readonly ReadOnlyDictionary<RailQualityPreset, RailQuality> PRESETS = new ReadOnlyDictionary<RailQualityPreset, RailQuality>(new Dictionary<RailQualityPreset, RailQuality> {
-            { RailQualityPreset.Vanilla, new RailQuality(0.7f, 0.7f) },
-            { RailQualityPreset.HighSpeedCorridor, new RailQuality(0.1f, 0.7f) },
-            { RailQualityPreset.DisusedBranchLine, new RailQuality(1.2f, 1.1f) }
+            { RailQualityPreset.Vanilla, new RailQuality(0.07f, 0.07f, 0.07f, 1.00f) },
+            { RailQualityPreset.HighSpeedCorridor, new RailQuality(0.03f, 0.03f, 0.03f, 0.50f) },
+            { RailQualityPreset.DisusedBranchLine, new RailQuality(0.20f, 0.20f, 0.20f, 3.00f) }
         });
 
-        [Draw("Kink Scale", Tooltip = "How much to scale the kinks of the track.")]
+        [Draw("Kink Scale", Tooltip = "The maximum offset of the kinks along the track.")]
         public float KinkScale;
-        [Draw("Kink Frequency", Tooltip = "How often kinks show up in the track.")]
+        [Draw("Kink Frequency", Tooltip = "How often kinks show up along the track.")]
         public float KinkFrequency;
+        [Draw("Vertical Kink Scale", Tooltip = "Extra offsetting vertically")]
+        public float VerticalKinkScale;
+        [Draw("Rotation Kink Scale", Tooltip = "Extra rotation")]
+        public float RotationKinkScale;
 
-        public RailQuality(float kinkScale, float kinkFrequency)
+        public RailQuality(float kinkScale, float kinkFrequency, float verticalKinkScale, float rotationKinkScale)
         {
             KinkScale = kinkScale;
             KinkFrequency = kinkFrequency;
+            VerticalKinkScale = verticalKinkScale;
+            RotationKinkScale = rotationKinkScale;
         }
         
         public bool IsVanilla()
         {
-            return Mathf.Approximately(KinkScale, PRESETS[RailGaugePreset.Vanilla].KinkScale) && Mathf.Approximately(KinkFrequency, PRESETS[RailGaugePreset.Vanilla].KinkFrequency);
+            return Mathf.Approximately(KinkScale, PRESETS[RailGaugePreset.Vanilla].KinkScale) &&
+                Mathf.Approximately(KinkFrequency, PRESETS[RailGaugePreset.Vanilla].KinkFrequency) &&
+                Mathf.Approximately(VerticalKinkScale, PRESETS[RailGaugePreset.Vanilla].VerticalKinkScale) &&
+                Mathf.Approximately(RotationKinkScale, PRESETS[RailGaugePreset.Vanilla].RotationKinkScale);
         }
     }
 }
