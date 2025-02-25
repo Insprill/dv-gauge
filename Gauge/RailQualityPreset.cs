f99ef41cf53cec5259ad2a638c566b3551fc6c9f
WhistleWiz <wizardmarcos@gmail.com>
2025-02-25 06:33:47 +0000

    Rail material (#6)
    * Create RailQualityPreset.cs
    
    * Create RailQuality.cs
    
    * Update Settings.cs
    
    * Update RailQuality.cs
    
    * Update RailwayMeshGeneratorPatch.cs
    
    * Update RailQuality.cs
    
    * Update Settings.cs
    
    * Fixed enum name.
    Increased Disused preset badness by 50%.
    
    * Reduced badness back down, it was too much.
    
    * Ajusted presets again.
    
    * Fixes meshes not updating until save is reloaded.
    
    * Added a setting to change rail joint distance (it's just the sound effect).
    
    * Allow changing the rail material.
    
    * Allow changing the rail material.

--- Gauge/RailQualityPreset.cs
+++ Gauge/RailQualityPreset.cs
@@ -8,6 +8,14 @@ namespace Gauge
         DisusedBranchLine = 3
     }
 
+    public enum RailMaterial
+    {
+        Default,
+        New,
+        Medium,
+        Old
+    }
+
     public static class RailQualityPresetExtensions
     {
         public static RailQuality RailQuality(this RailQualityPreset preset)
